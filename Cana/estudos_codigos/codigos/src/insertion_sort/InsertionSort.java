744da8f8253f69cd8de000db0939b8d602d8ab8e
Savas <savas.neto20@gmail.com>
2025-08-11 17:46:17 -0300

    fix: method errors

--- Cana/estudos_codigos/codigos/src/insertion_sort/InsertionSort.java
+++ Cana/estudos_codigos/codigos/src/insertion_sort/InsertionSort.java
@@ -30,15 +30,15 @@ public class InsertionSort {
 
     public static void insertionSort(int[] lista) {
         for (int i = 1; i < lista.length; i++) {
-            int currentValue = lista[i];
+            int current = lista[i];
+
             int j = i - 1;
 
-            while (j >= 0 && lista[j] > currentValue) {
+            while (j >= 0 && lista[j] > current) {
                 lista[j + 1] = lista[j];
                 j--;
             }
-            lista[j + 1] = currentValue;
+            lista[j + 1] = current;
         }
-
     }
 }
