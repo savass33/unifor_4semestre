edc29c975f477f4a1b0d391a85a8f423c64b8f01
Savas <savas.neto20@gmail.com>
2025-08-11 13:41:27 -0300

    first commit - 4o semestre

---
+++ Met_quant/Exercicios_Python/loops.py
@@ -0,0 +1,23 @@
+x = 12
+i = 1
+""" 
+# Loop to print multiplication table of x
+for i in range(1, 11):
+    x += 1
+    print(x)
+ """
+""" lista = [1, 2, 3, 4, 5]
+
+for valor in lista:
+    print(valor)
+    print("Fim da lista") """
+
+boletim = {
+    'Portugues': 7.5,
+    'Matematica': 8.0, 
+    'Historia': 6.5,
+    'Geografia': 9.0,
+}
+
+for chave, valor in boletim.items():
+    print (f"{chave}: {valor}")
