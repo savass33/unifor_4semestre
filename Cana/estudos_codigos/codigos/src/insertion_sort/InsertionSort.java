8b29daa1e3f22cb7a861f1b3d2f9acfad42641fb
Savas <savas.neto20@gmail.com>
2025-08-11 15:08:17 -0300

    add: insertionsort method

--- Cana/estudos_codigos/codigos/src/insertion_sort/InsertionSort.java
+++ Cana/estudos_codigos/codigos/src/insertion_sort/InsertionSort.java
@@ -1,7 +1,44 @@
 package insertion_sort;
 
+import java.util.Random;
+
 public class InsertionSort {
     public static void main(String[] args) throws Exception {
+        Random random = new Random();
+        int[] lista = new int[10];
+
+        // Preenchendo a lista com números aleatórios
+        for (int i = 0; i < lista.length; i++) {
+            lista[i] = random.nextInt(100); // Números aleatórios entre 0 e 99
+        }
+
+        System.out.println("Lista original: ");
+        imprimeLista(lista);
+
+    }
+
+    public static void imprimeLista(int[] lista) {
+        for (int i = 0; i < lista.length; i++) {
+            System.out.print(lista[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void insertionSort(int[] lista) {
+        for (int i = 1; i < lista.length; i++) {
+            int currentValue = lista[i];
+            int j = i - 1;
+
+            while (j != 0 && lista[j] > currentValue) {
+                {
+                    lista[j + 1] = lista[j];
+                    j--;
+                }
+
+                lista[j - 1] = currentValue;
+
+            }
+        }
 
     }
 }
